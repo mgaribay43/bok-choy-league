226ce2506e6606ebd9f91e90dc8db82ee821d465
mgaribay43 <mikeyjordan43@gmail.com>
2025-06-30 21:55:44 -0400

    -adjusted rules doc sizing

--- app/components/googledocviewer.tsx
+++ app/components/googledocviewer.tsx
@@ -9,15 +9,29 @@ type GoogleDocViewerProps = {
 const GoogleDocViewer = ({ docId }: GoogleDocViewerProps) => {
   const embedUrl = `https://docs.google.com/document/d/${docId}/preview`;
 
+const viewUrl = `https://docs.google.com/document/d/${docId}/view`;
+
   return (
     <div className="w-full max-w-5xl mx-auto px-4 py-6">
-      <div className="relative w-full pt-[140%] sm:pt-[100%] md:pt-[80%] lg:pt-[65%] xl:pt-[56.25%]">
+      {/* Responsive iframe container */}
+      <div className="relative w-full h-[90vh] sm:h-[85vh] md:h-[80vh]">
         <iframe
           src={embedUrl}
           className="absolute top-0 left-0 w-full h-full rounded-md border border-gray-300"
           allowFullScreen
         ></iframe>
       </div>
+      {/* Optional View Fullscreen Link */}
+      <p className="text-center mt-4">
+        <a
+          href={viewUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-green-700 underline hover:text-green-900 font-medium"
+        >
+          📄 View in fullscreen ↗
+        </a>
+      </p>
     </div>
   );
 };

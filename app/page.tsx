6a885f174420771f39447e53652a854705e799f3
mgaribay43 <mikeyjordan43@gmail.com>
2025-08-19 00:39:29 -0400

    added keepers marquee

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Ices from './components/Ices';
 import Standings from './components/Standings';
 import Events from './components/Events';
+import KeeperBanner from './components/KeeperMarquee'
 
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
+      <KeeperBanner/>
       <Standings topThree/>
       <Events eventsSlideshow />
       <Ices latestOnly />

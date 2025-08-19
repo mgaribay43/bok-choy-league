cedf71bf23ad226c5ff84a4dea12169e3410840f
mgaribay43 <mikeyjordan43@gmail.com>
2025-08-18 22:36:29 -0400

    rules page preliminary commit

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,10 +1,10 @@
 import GoogleDocViewer from '@/app/components/googledocviewer';
+import RulesPage from '../components/Rules'
 
 export default function LeagueRulesPage() {
   return (
     <div className="min-h-screen py-10 px-6">
-      <h1 className="text-3xl font-bold text-center text-green-800 mb-6">📄 League Rules</h1>
-      <GoogleDocViewer docId="1gj0O8C0uGSuRfdKOp8DtMYaxPpKued1vfTOysgBe75Q" />
+      <RulesPage />
     </div>
   );
 }

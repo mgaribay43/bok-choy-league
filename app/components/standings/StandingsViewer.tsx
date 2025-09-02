17ca451859cc20230d10b3cccbcaefed17b71f04
Mike <mikeyjordan43@gmail.com>
2025-09-02 13:14:29 -0400

    removed unecessart imports

--- app/components/standings/StandingsViewer.tsx
+++ app/components/standings/StandingsViewer.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
 
 import { TeamEntry, StandingsProps, getDisplayManagerName } from "./utils/standingsUtils";
 
--- app/components/standings/utils/standingsUtils.ts
+++ app/components/standings/utils/standingsUtils.ts
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export interface TeamEntry {
   id: string;
   name: string;
